ce45fe83f3f06ca24ffc85944f0d0eb85f6c0c9c
Neha Patel <nehajp999@gmail.com>
2020-06-09 23:48:18 +0100

    operators

---
+++ src/Operator.java
@@ -0,0 +1,19 @@
+public class Operator {
+    static int X = 100;
+    static int Y = 200;
+
+    static void add (){
+        System.out.println("Addition = "+ X+Y);
+
+    }
+    static void sub(){
+        System.out.println(( "Subtraction = ")+ (X-Y));
+
+    }
+    public static void main(String[] args) {
+    add ();
+    sub();
+    System.out.println(X+Y);
+
+    }
+}
